5f3788fb5cbf2dbaec7d9f32a852feaf0602dc81
Nihalrathore03 <nihalrathore0207@gmail.com>
2025-01-03 17:54:14 +0000

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+   // console.log(props);
+  return (
+    <div>
+      Card
+      <h2> hellow from  </h2>
+      <h4> location</h4>
+      <h4> age</h4>
+      <h4> profession</h4>
+      
+    </div>
+  );
+};
+
+export default Card;
